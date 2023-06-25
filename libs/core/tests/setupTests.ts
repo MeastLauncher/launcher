1bb81dae5f6f6be6bb1e67ddd91e2b2932691d05
CommandMaker <command.maker83@gmail.com>
2023-06-25 18:04:49 +0200

    fix(core): tests fail because of copying the config files before each

--- libs/core/tests/setupTests.ts
+++ libs/core/tests/setupTests.ts
@@ -2,8 +2,10 @@ import { join } from 'path';
 import { copyFileSync } from 'fs';
 import { beforeEach } from 'vitest';
 
-beforeEach(() => {
+beforeEach(async () => {
     /* Reset test application's configuration */
     copyFileSync(join(__dirname, './config/test_files/config_default.json'), join(__dirname, './config/test_files/config.json'));
     copyFileSync(join(__dirname, './config/test_files/data_default.json'), join(__dirname, './config/test_files/data.json'));
+
+    await new Promise((r) => setTimeout(r, 100));
 });
